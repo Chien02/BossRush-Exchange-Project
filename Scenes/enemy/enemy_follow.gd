7c295bbcdff7319cd4cf61716b42ddb92286c15e
ThanhToan <codehub.personal@gmail.com>
2024-01-22 10:46:07 +0700

    update enemy

---
+++ Scenes/enemy/enemy_follow.gd
@@ -0,0 +1,23 @@
+extends State
+class_name EnemyFollow
+
+@export var enemy: CharacterBody2D
+@export var move_speed := 30.0
+@export var distance := 50
+var player: CharacterBody2D
+
+func enter():
+	player = get_tree().get_first_node_in_group("Player")
+
+func physicsUpdate(delta: float):
+	var direction = player.global_position - enemy.global_position
+	print("test===========")
+	print(direction.length())
+	if direction.length() < 70 :
+		enemy.velocity = direction.normalized() * move_speed
+	#if direction.length() > 50:
+		#print("test == Idle")
+	else:
+		enemy.velocity = Vector2()
+		
+	
